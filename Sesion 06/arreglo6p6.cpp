9e4bee376619a4d318a1139c49b9f53f233c1fd7
Josue Vargas <jmvargasmatute@gmail.com>
2026-07-17 00:35:00 -0500

    Add files via upload

---
+++ Sesion 06/arreglo6p6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+suma += i "";
+return suma;
+
+
+
+
+estudiante1[4]= {};
+for (i = 0; i<3; i++){
+    suma1 += i;
+    promedio = suma1 /4;
+    return promedio;
+
+}
+
+for ()
+
